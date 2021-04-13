74a76159650f853fbe6ebe445bfc423459c52c09
Alisson Rangel <alisson.rangel@hotmail.com>
2021-04-13 14:00:23 -0300

    Adiciona listagem de conteúdos

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   root to: "home#index"
   get 'home/index'
   get 'home', to: "home#index"
+
+  resources :contents
+  # get 'contents', to: "contents#index"
+  # get 'contents/new', to: "contents#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
