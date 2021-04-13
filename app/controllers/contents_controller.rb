f4a52a5c722ec05b923bd1b251a975b86a31836d
Alisson Rangel <alisson.rangel@hotmail.com>
2021-04-13 16:06:47 -0300

    Permite atualizacao e visualizacao de conteudo

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -26,6 +26,22 @@ class ContentsController < ApplicationController
     @content = Content.find(params[:id])
   end
 
+  def update
+    # byebug
+
+    @content = Content.find(params[:id])
+
+    if @content.update(content_params)
+      redirect_to contents_path, notice: 'Content successfully updated!'
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @content = Content.find(params[:id])
+  end
+
   private
 
   def content_params
