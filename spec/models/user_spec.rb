6b142f8b9478b6c67bfd3d770b0b1799aeb23b17
Alisson Rangel <alisson.rangel@hotmail.com>
2021-04-15 21:10:19 -0300

    Finalização do app, com mais testes, alteração das cores das tags com diferentes pesos e aplicação do efeito matrix

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:contents) }
+    it { should have_many(:tags) }
+  end
+
+  describe 'validations' do
+    it { should validate_uniqueness_of(:email).case_insensitive }
+  end
 end
